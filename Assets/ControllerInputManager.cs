0725b93c2fdeea48f46d00e3bb00eb54bf3c1310
JamesGuenther <lordcheetah@comcast.net>
2017-05-02 16:09:57 -0400

    Feat: Teleportation

--- Assets/ControllerInputManager.cs
+++ Assets/ControllerInputManager.cs
@@ -16,17 +16,6 @@ public class ControllerInputManager : MonoBehaviour
 
 	public float yNudgeAmount = 1f; //specific to teleportAimerObject height
 
-	//dash
-	public float dashSpeed = 0.2f;
-	private bool isDashing = false;
-	private float lerpTime;
-	private Vector3 dashStartPosition;
-
-	//Walking
-	public Transform playerCam;
-	public float moveSpeed = 4f;
-	private Vector3 movementDirection;
-
 	//swipe
 	public float swipeSum;
 	public float touchLast;
@@ -48,68 +37,11 @@ public class ControllerInputManager : MonoBehaviour
 	{
 		device = SteamVR_Controller.Input ((int)trackedObj.index);
 
-		if (device.GetPress (SteamVR_Controller.ButtonMask.Grip))
-		{
-			movementDirection = playerCam.transform.forward;
-			movementDirection = new Vector3 (movementDirection.x, 0, movementDirection.z);
-			movementDirection *= moveSpeed * Time.deltaTime;
-			player.transform.position += movementDirection;
-		}
-
-		if (isDashing)
-		{
-			lerpTime = 1 * dashSpeed;
-			player.transform.position = Vector3.Lerp (dashStartPosition, teleportLocation, lerpTime);
-			if (lerpTime >= 1)
-			{
-				isDashing = false;
-				lerpTime = 0;
-			}
-		}
-
-		if (device.GetPress (SteamVR_Controller.ButtonMask.Trigger))
-		{
-			laser.gameObject.SetActive (true);
-			teleportAimerObject.SetActive (true);
-
-			laser.SetPosition (0, gameObject.transform.position);
-			RaycastHit hit;
-			if (Physics.Raycast (transform.position, transform.forward, out hit, 15, laserMask))
-			{
-				teleportLocation = hit.point;
-				laser.SetPosition (1, teleportLocation);
-				//aimer position
-				teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
-			}
-			else
-			{
-				teleportLocation = new Vector3 (transform.forward.x * 15 + transform.forward.x, transform.forward.y * 15 + transform.position.y, transform.forward.z * 15 + transform.position.z);
-				RaycastHit groundRay;
-				if (Physics.Raycast (teleportLocation, -Vector3.up, out groundRay, 17, laserMask))
-				{
-					teleportLocation = new Vector3 (transform.forward.x * 15 + transform.position.x, groundRay.point.y, transform.forward.z * 15 + transform.position.z);
-
-				}
-				laser.SetPosition (1, transform.forward * 15 + transform.position);
-				//aimer position
-				teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
-			}
-		}
-
-		if (device.GetPressUp (SteamVR_Controller.ButtonMask.Trigger))
-		{
-			laser.gameObject.SetActive (false);
-			teleportAimerObject.SetActive (false);
-			//player.transform.position = teleportLocation;
-			dashStartPosition = player.transform.position;
-			isDashing = true;
-		}
-
 		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Touchpad))
 		{
-			//SteamVR_LoadLevel.Begin (Scene1);
 			touchLast = device.GetAxis (Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
 		}
+
 		if (device.GetTouch (SteamVR_Controller.ButtonMask.Touchpad))
 		{
 			touchCurrent = device.GetAxis (Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
@@ -117,19 +49,10 @@ public class ControllerInputManager : MonoBehaviour
 			touchLast = touchCurrent;
 			swipeSum += distance;
 
-			if (!hasSwipedRight)
-			{
-				if (swipeSum > 0.5f)
-				{
-					swipeSum = 0;
-					SwipedRight ();
-					hasSwipedRight = true;
-					hasSwipedLeft = false;
-				}
-			}
+
 			if (!hasSwipedLeft)
 			{
-				if(swipeSum < -0.5f)
+				if (swipeSum < -0.5f)
 				{
 					swipeSum = 0;
 					SwipedLeft ();
@@ -139,6 +62,7 @@ public class ControllerInputManager : MonoBehaviour
 			}
 
 		}
+
 		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Touchpad))
 		{
 			swipeSum = 0;
@@ -146,60 +70,43 @@ public class ControllerInputManager : MonoBehaviour
 			touchLast = 0;
 			hasSwipedLeft = false;
 			hasSwipedRight = false;
+
+			laser.gameObject.SetActive (false);
+			teleportAimerObject.SetActive (false);
+			player.transform.position = teleportLocation;
 		}
-		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad))
-		{
-			//spawn object currently selected by menu
-			SpawnObject();
-		}
-	}
 
-	void SpawnObject()
-	{
-		//objectMenuManager.SpawnCurrentObject ();
-	}
 
-	void OnTriggerStay(Collider col)
-	{
-		if (col.gameObject.CompareTag ("Throwable"))
-		{
-			if (device.GetPressUp (SteamVR_Controller.ButtonMask.Trigger))
-			{
-				ThrowObject (col);
-			} else if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger))
-			{
-				GrabObject (col);
-			}
-		}
-	}
-
-	void GrabObject(Collider coli)
-	{
-		coli.transform.SetParent (gameObject.transform);
-		coli.GetComponent<Rigidbody> ().isKinematic = true;
-		device.TriggerHapticPulse (2000);
-		Debug.Log ("You are touching down the trigger on an object");
-	}
-
-	void ThrowObject(Collider coli)
-	{
-		coli.transform.SetParent (null);
-		Rigidbody rigidBody = coli.GetComponent<Rigidbody> ();
-		rigidBody.isKinematic = false;
-		rigidBody.velocity = device.velocity * throwForce;
-		rigidBody.angularVelocity = device.angularVelocity;
-		Debug.Log ("You have released the trigger");
 	}
 
 	void SwipedLeft()
 	{
-		//objectMenuManager.MenuLeft ();
+		laser.gameObject.SetActive (true);
+		teleportAimerObject.SetActive (true);
+
+		laser.SetPosition (0, gameObject.transform.position);
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, 15, laserMask))
+		{
+			teleportLocation = hit.point;
+			laser.SetPosition (1, teleportLocation);
+			//aimer position
+			teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
+		}
+		else
+		{
+			teleportLocation = new Vector3 (transform.forward.x * 15 + transform.forward.x, transform.forward.y * 15 + transform.position.y, transform.forward.z * 15 + transform.position.z);
+			RaycastHit groundRay;
+			if (Physics.Raycast (teleportLocation, -Vector3.up, out groundRay, 17, laserMask))
+			{
+				teleportLocation = new Vector3 (transform.forward.x * 15 + transform.position.x, groundRay.point.y, transform.forward.z * 15 + transform.position.z);
+
+			}
+			laser.SetPosition (1, transform.forward * 15 + transform.position);
+			//aimer position
+			teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
+		}
 		Debug.Log ("SwipeLeft");
 	}
 
-	void SwipedRight()
-	{
-		//objectMenuManager.MenuRight ();
-		Debug.Log ("SwipeRight");
-	}
 }
