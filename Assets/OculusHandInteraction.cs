e3aab244741302ad8de993e53a24804c4c57cbf7
JamesGuenther <lordcheetah@comcast.net>
2017-05-12 21:30:21 -0400

    Latest try to get Oculus Touch to work

--- Assets/OculusHandInteraction.cs
+++ Assets/OculusHandInteraction.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OculusHandInteraction : MonoBehaviour {
+	private OVRInput.Controller thisController;
+	public bool leftHand; // true if left hand controller
+
+	public float throwForce = 1.5f;
+
+	public enum VibrationForce
+	{
+		Light,
+		Medium,
+		Hard,
+	}
+
+	[SerializeField]
+	OVRInput.Controller controllerMask;
+
+	private OVRHapticsClip clipLight;
+	private OVRHapticsClip clipMedium;
+	private OVRHapticsClip clipHard;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (leftHand)
+		{
+			thisController = OVRInput.Controller.LTouch;
+		} else
+		{
+			thisController = OVRInput.Controller.RTouch;
+		}
+		InitializeOVRHaptics();
 	}
 	
 	// Update is called once per frame
@@ -14,11 +39,73 @@ public class OculusHandInteraction : MonoBehaviour {
 		
 	}
 
-	void OnTriggerStay(Collider target)
+	void OnTriggerStay(Collider col)
 	{
-		if (target.tag == "Throwable")
+		if (col.gameObject.CompareTag ("Throwable"))
 		{
-			// throw ball
+			if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, thisController) < 0.1f)
+			{
+				ThrowObject (col);
+			} else if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, thisController) > 0.1f)
+			{
+				GrabObject (col);
+			}
+		}
+	}
+
+	void GrabObject(Collider coli)
+	{
+		coli.transform.SetParent (gameObject.transform);
+		coli.GetComponent<Rigidbody> ().isKinematic = true;
+		Vibrate (VibrationForce.Light);
+		Debug.Log ("You are touching down the trigger on an object");
+	}
+
+	void ThrowObject(Collider coli)
+	{
+		coli.transform.SetParent (null);
+		Rigidbody rigidBody = coli.GetComponent<Rigidbody> ();
+		rigidBody.isKinematic = false;
+		rigidBody.velocity = OVRInput.GetLocalControllerVelocity (thisController) * throwForce;
+		rigidBody.angularVelocity = OVRInput.GetLocalControllerAngularVelocity (thisController);
+		Debug.Log ("You have released the trigger");
+	}
+
+	private void InitializeOVRHaptics()
+	{
+		int cnt = 10;
+		clipLight = new OVRHapticsClip(cnt);
+		clipMedium = new OVRHapticsClip(cnt);
+		clipHard = new OVRHapticsClip(cnt);
+		for (int i = 0; i < cnt; i++)
+		{
+			clipLight.Samples[i] = i % 2 == 0 ? (byte)0 : (byte)75;
+			clipMedium.Samples[i] = i % 2 == 0 ? (byte)0 : (byte)150;
+			clipHard.Samples[i] = i % 2 == 0 ? (byte)0 : (byte)255;
+		}
+
+		clipLight = new OVRHapticsClip(clipLight.Samples, clipLight.Samples.Length);
+		clipMedium = new OVRHapticsClip(clipMedium.Samples, clipMedium.Samples.Length);
+		clipHard = new OVRHapticsClip(clipHard.Samples, clipHard.Samples.Length);
+	}
+
+	public void Vibrate(VibrationForce vibrationForce)
+	{
+		var channel = OVRHaptics.RightChannel;
+		if (leftHand)
+			channel = OVRHaptics.LeftChannel;
+
+		switch (vibrationForce)
+		{
+		case VibrationForce.Light:
+			channel.Preempt(clipLight);
+			break;
+		case VibrationForce.Medium:
+			channel.Preempt(clipMedium);
+			break;
+		case VibrationForce.Hard:
+			channel.Preempt(clipHard);
+			break;
 		}
 	}
 }
