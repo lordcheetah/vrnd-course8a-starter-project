7a7f37329189bf8682851e4a50bb74ef28e7ce7a
JamesGuenther <lordcheetah@comcast.net>
2017-05-18 17:33:56 -0400

    Feat: Structure parts

--- Assets/OculusHandInteraction.cs
+++ Assets/OculusHandInteraction.cs
@@ -6,7 +6,18 @@ public class OculusHandInteraction : MonoBehaviour {
 	private OVRInput.Controller thisController;
 	public bool leftHand; // true if left hand controller
 
+	public GameObject teleportAimerObject;
+	public Vector3 teleportLocation;
+	public GameObject player;
+	public LayerMask laserMask;
+
 	public float throwForce = 1.5f;
+
+	private LineRenderer laser;
+
+
+
+	public float yNudgeAmount = 1f; //specific to teleportAimerObject height
 
 	public enum VibrationForce
 	{
@@ -22,6 +33,8 @@ public class OculusHandInteraction : MonoBehaviour {
 	private OVRHapticsClip clipMedium;
 	private OVRHapticsClip clipHard;
 
+	private float menuStickX;
+
 	// Use this for initialization
 	void Start () {
 		if (leftHand)
@@ -32,11 +45,46 @@ public class OculusHandInteraction : MonoBehaviour {
 			thisController = OVRInput.Controller.RTouch;
 		}
 		InitializeOVRHaptics();
+		laser = GetComponentInChildren<LineRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		menuStickX = OVRInput.Get (OVRInput.Axis2D.PrimaryThumbstick, thisController).x;
+		if (leftHand && menuStickX < 0.45f && menuStickX > -0.45f)
+		{
+			laser.gameObject.SetActive (true);
+			teleportAimerObject.SetActive (true);
+
+			laser.SetPosition (0, gameObject.transform.position);
+			RaycastHit hit;
+			if (Physics.Raycast (transform.position, transform.forward, out hit, 15, laserMask))
+			{
+				teleportLocation = hit.point;
+				laser.SetPosition (1, teleportLocation);
+				//aimer position
+				teleportAimerObject.transform.position = new Vector3 (teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
+			} else
+			{
+				teleportLocation = new Vector3 (transform.forward.x * 15 + transform.forward.x, transform.forward.y * 15 + transform.position.y, transform.forward.z * 15 + transform.position.z);
+				RaycastHit groundRay;
+				if (Physics.Raycast (teleportLocation, -Vector3.up, out groundRay, 17, laserMask))
+				{
+					teleportLocation = new Vector3 (transform.forward.x * 15 + transform.position.x, groundRay.point.y, transform.forward.z * 15 + transform.position.z);
+
+				}
+				laser.SetPosition (1, transform.forward * 15 + transform.position);
+				//aimer position
+				teleportAimerObject.transform.position = teleportLocation + new Vector3 (0, yNudgeAmount, 0);
+			}
+		}
+			
+		if (leftHand && menuStickX >= 0.45f && menuStickX <= -0.45f)
+		{
+			laser.gameObject.SetActive (false);
+			teleportAimerObject.SetActive (false);
+			player.transform.position = teleportLocation;
+		}
 	}
 
 	void OnTriggerStay(Collider col)
@@ -64,10 +112,16 @@ public class OculusHandInteraction : MonoBehaviour {
 	void ThrowObject(Collider coli)
 	{
 		coli.transform.SetParent (null);
-		Rigidbody rigidBody = coli.GetComponent<Rigidbody> ();
-		rigidBody.isKinematic = false;
-		rigidBody.velocity = OVRInput.GetLocalControllerVelocity (thisController) * throwForce;
-		rigidBody.angularVelocity = OVRInput.GetLocalControllerAngularVelocity (thisController);
+		if (coli.tag == "Structure")
+		{
+			coli.enabled = false;
+		} else
+		{
+			Rigidbody rigidBody = coli.GetComponent<Rigidbody> ();
+			rigidBody.isKinematic = false;
+			rigidBody.velocity = OVRInput.GetLocalControllerVelocity (thisController) * throwForce;
+			rigidBody.angularVelocity = OVRInput.GetLocalControllerAngularVelocity (thisController);
+		}
 		Debug.Log ("You have released the trigger");
 	}
 
